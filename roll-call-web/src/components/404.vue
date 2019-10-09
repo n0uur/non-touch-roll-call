e741e00e795b90818155c690a9f2f13115ab5b2c
n0uur <50010805+n0uur@users.noreply.github.com>
2019-10-09 22:21:27 +0700

    Add CookieJS to VueJS and Update some Components

---
+++ roll-call-web/src/components/404.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h4>404 NOT FOUND</h4>{{ msg }}
+  </div>
+</template>
+
--- roll-call-web/src/router/index.js
+++ roll-call-web/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import Cookies from 'js-cookie'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'Login',
+      mode: 'history'
+      // component: () => import('@/components/HelloWorld')
+    },
+    {
+      path: '*',
+      name: '404 Page Not Found'
+      // component: () => import('./pathToComponent'),
     }
   ]
 })
