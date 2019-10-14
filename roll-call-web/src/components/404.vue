178bc73249b39f768a9dc4330384b27719d48c64
n0uur <50010805+n0uur@users.noreply.github.com>
2019-10-15 00:42:15 +0700

    Fixed Responsive

--- roll-call-web/src/components/404.vue
+++ roll-call-web/src/components/404.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div style="text-align: center">
     <div class="notfound">
       <div class="nt404">4&nbsp;&nbsp;&nbsp;4</div>
       <span class="egg"></span>
     </div>
     <br>
-    <h1>ไม่พบหน้าที่คุณร้องขอ</h1>
-    <h5>หากคิดว่านี่คือข้อผิดพลาดของระบบ กรุณาติดต่อผู้ดูแลระบบ</h5>
+    <h2>ไม่พบหน้าที่คุณร้องขอ</h2>
+    <p>หากคิดว่านี่คือข้อผิดพลาด กรุณาติดต่อผู้ดูแลระบบ</p>
     <button class="btn btn-info" @click="$router.push('/')">กลับสู่หน้าหลัก</button>
   </div>
 </template>
@@ -14,17 +14,16 @@
 <style>
 .nt404 {
   display: inline-block;
-  position:relative;
-  bottom: -100px;
+  position: relative;
+  bottom: -80px;
   right: 14px;
 }
 .notfound {
-  font-size: 140px;
+  font-size: 120px;
   margin: auto;
-  display: inline-block;
   background-image: url('./assets/fried-egg.png');
   background-size: cover;
-  width: 400px;
-  height: 400px;
+  width: 320px;
+  height: 320px;
 }
 </style>
