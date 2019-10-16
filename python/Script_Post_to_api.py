5dea3dd89f36319d5142510236f47098a1ad4a58
boatlittlebear <56269911+boatlittlebear@users.noreply.github.com>
2019-10-17 00:59:13 +0700

    Check information before sending it on the web.

--- python/Script_Post_to_api.py
+++ python/Script_Post_to_api.py
@@ -4,14 +4,25 @@ import requests
 def script():
     while True:
         data = input()
-        param = {"ID": data}
-        res = requests.post("https://api-t2o.ktnis.me", params = param)
-        if res.text == "200":
-            print("Success")
-            for _ in range(2):
-                winsound.Beep(2500, 100)
-        else:
+        allnumber = "1234567890"
+        count = 0
+        for number in data:
+            if number not in allnumber or len(data) != 10:
+                count += 1
+        if count > 0:
             print("Error")
             winsound.Beep(2500, 1500)
+            count = 0
+        else:
+            param = {"ID": data}
+            res = requests.post("https://api-t2o.ktnis.me", params = param)
+            if res.text == "200":
+                print("Success")
+                for _ in range(2):
+                 winsound.Beep(2500, 250)
+            else:
+                print("Error")
+                winsound.Beep(2500, 1500)
+            count = 0
     
 script()
