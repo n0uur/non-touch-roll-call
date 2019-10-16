d08400a66945b7d79c78ec579099f9a61a114055
boatlittlebear <56269911+boatlittlebear@users.noreply.github.com>
2019-10-17 00:16:12 +0700

    เพิ่มอินพุท

--- python/Script_Post_to_api.py
+++ python/Script_Post_to_api.py
@@ -1,9 +1,9 @@
-"""Scrip_Post_to_api"""
+"""Script_Post_to_api"""
 import winsound
 import requests
-def scrip():
-    data = str(list(range(20)))
-    param = {"key": data}
+def script():
+    data = input()
+    param = {"ID": data}
     res = requests.post("https://api-t2o.ktnis.me", params = param)
     if res.text == "200":
         print("Success")
@@ -13,4 +13,4 @@ def scrip():
         print("Error")
         winsound.Beep(2500, 1500)
     
-scrip()
+script()
