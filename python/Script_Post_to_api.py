4d0edba2610a2800126ccd48c8077b26ef20a3dc
boatlittlebear <56269911+boatlittlebear@users.noreply.github.com>
2019-10-17 00:26:03 +0700

    Update Script_Post_to_api.py

--- python/Script_Post_to_api.py
+++ python/Script_Post_to_api.py
@@ -2,15 +2,16 @@
 import winsound
 import requests
 def script():
-    data = input()
-    param = {"ID": data}
-    res = requests.post("https://api-t2o.ktnis.me", params = param)
-    if res.text == "200":
-        print("Success")
-        for _ in range(2):
-            winsound.Beep(2500, 500)
-    else:
-        print("Error")
-        winsound.Beep(2500, 1500)
+    while True:
+        data = input()
+        param = {"ID": data}
+        res = requests.post("https://api-t2o.ktnis.me", params = param)
+        if res.text == "200":
+            print("Success")
+            for _ in range(2):
+                winsound.Beep(2500, 100)
+        else:
+            print("Error")
+            winsound.Beep(2500, 1500)
     
 script()
