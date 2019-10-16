0d31a440527c09a187419f2c9dc69c554c61b117
boatlittlebear <56269911+boatlittlebear@users.noreply.github.com>
2019-10-16 12:47:07 +0700

    ทกสอยยิง api ขึ้น server ด้วน python

--- python/Script_Post_to_api.py
+++ python/Script_Post_to_api.py
@@ -1,1 +1,11 @@
-
+"""Scrip_Post_to_api"""
+import requests
+def main():
+    data = str(list(range(20)))
+    param = {"key": data}
+    res = requests.post("https://api-t2o.ktnis.me", params = param)
+    if res.text == "200":
+        print("Success")
+    else:
+        print("Error")
+main()
