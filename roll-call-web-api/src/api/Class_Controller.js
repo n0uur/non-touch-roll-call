72d548655f647627de68d33c6d3e9adf5a637259
n0uur <50010805+n0uur@users.noreply.github.com>
2019-11-20 00:35:46 +0700

    add crypto, create classroom, get classroom

--- roll-call-web-api/src/api/Class_Controller.js
+++ roll-call-web-api/src/api/Class_Controller.js
@@ -1,43 +1,78 @@
 var express = require('express')
 var ClassController = express()
 const conn = require('./mysql')
+const crypto = require('crypto');
 
-ClassController.post('/addClass', async (request, response) => {
-    try {
-        let class_info = request.body;
-        console.log(class_info)
-    } catch (e) {
-        console.log(e);
-        response.send({
-            statusCode: 500,
-            status: false,
-            message: 'Internal Server Error',
-            err: e
-        });
-    }
-});
+function SHA256(paintext) {
+    return crypto.createHash('sha256').update(paintext).digest('hex');
+}
 
-ClassController.get('/', (req, res) => {
-    query = "SELECT * from student_data"
+ClassController.get('/get/:classid', (req, res) => {
+    const { classid } = req.params
 
-    conn.query(query, (error, results, fields) => {
+    conn.query("SELECT * from classroom_data WHERE Class_ID = ?", [classid], (error, results, fields) => {
         if (error) {
             console.log(error)
+            res.status(500).send()
         }
-
-        res.header("Content-Type", 'application/json')
-        res.status(200).send(JSON.stringify(results, null, 2))
+        else {
+            res.header("Content-Type", 'application/json')
+            res.status(200).send(JSON.stringify(results[0], null, 2))
+        }
     })
-    
-    // conn.query("", (error, results, fields) => {
-    //     if (error) {
-    //         console.log(error)
-    //     }
-    //     console.log(results)
-    //     res.header("Content-Type", 'application/json')
-    //     res.send(JSON.stringify({ data: results }, null, 2))
-    // })
 })
 
+ClassController.get('/all', (req, res) => {
+    const { classid } = req.params
+
+    conn.query("SELECT * from classroom_data", (error, results, fields) => {
+        if (error) {
+            console.log(error)
+            res.status(500).send()
+        }
+        else {
+            res.header("Content-Type", 'application/json')
+            res.status(200).send(JSON.stringify(results, null, 2))
+        }
+    })
+})
+
+ClassController.post('/create', (req, res) => {
+    const { subject, instructor, password } = req.body
+
+    var generatedID = Math.floor(Math.random()*90000) + 10000
+    var hashPassword = SHA256(password)
+
+    console.log("Created Classroom:" + generatedID, subject, instructor)
+
+    conn.query("INSERT INTO classroom_data (Class_ID, Class_Subject, Class_Instructor, Class_Password) VALUE (?, ?, ?, ?)", [generatedID, subject, instructor, hashPassword], (error, results, fields) => {
+        if (error) {
+            console.log(error)
+            res.status(500).send()
+        }
+        else {
+            res.header("Content-Type", 'application/json')
+            res.status(200).send({status: '200', classid: generatedID})
+        }
+    })
+})
+
+ClassController.post('/close/:classid', (req, res) => {
+    const { password } = req.body
+    const { classid } = req.params
+
+    var hashPassword = SHA256(password)
+
+    conn.query("SELECT * from classroom_data WHERE Class_ID = ? and Class_Password = ?", [classid, hashPassword], (error, results, fields) => {
+        if (error) {
+            console.log(error)
+            res.status(500).send()
+        }
+        else {
+            res.header("Content-Type", 'application/json')
+            res.status(200).send({status: '200', classid: classid})
+        }
+    })
+})
 
 module.exports = ClassController;
