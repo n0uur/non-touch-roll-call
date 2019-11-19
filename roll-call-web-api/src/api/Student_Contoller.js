85674d8bb8e3505f318a58a955a95fb4ae6ca4dd
n0uur <50010805+n0uur@users.noreply.github.com>
2019-11-20 00:35:57 +0700

    add get student from CardID

--- roll-call-web-api/src/api/Student_Contoller.js
+++ roll-call-web-api/src/api/Student_Contoller.js
@@ -2,6 +2,23 @@ var express = require('express')
 var StudentController = express()
 const conn = require('./mysql')
 
+StudentController.get('/get/:cardid', (req, res) => {
+    try {
+        const { cardid } = req.params
+
+        conn.query("SELECT * from student_data WHERE STD_CardID = ?", [cardid], (error, results, fields) => {
+            if (error) {
+                console.log(error)
+            }
+            res.header("Content-Type", 'application/json')
+            res.status(200).send(JSON.stringify(results[0], null, 2))
+        })
+    }
+    catch (e) {
+        console.log(e)
+    }
+})
+
 StudentController.get('/all', (req, res) => {
     try {
         query = "SELECT * from student_data"
