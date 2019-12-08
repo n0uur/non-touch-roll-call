c6ee3beb44e4ef1e33e233429f725c183b8da4e3
boatlittlebear <56269911+boatlittlebear@users.noreply.github.com>
2019-12-08 14:22:06 +0700

    add function terminate_program
    add function terminate_program

--- python/Script_Post_to_api.py
+++ python/Script_Post_to_api.py
@@ -2,6 +2,7 @@
 import winsound
 import requests
 import calendar
+import sys
 import json
 from datetime import datetime
 
@@ -18,6 +19,8 @@ def stdreg():
     """This function is used for registration. By student ID and student ID"""
     while True:
         cardID = input()
+        if cardID == "End":
+            terminate_program()
         studentID = input()
         if cardID.isdigit() == True and len(cardID) == 10 \
             and studentID.isdigit() == True and len(studentID) == 8:
@@ -30,15 +33,18 @@ def attend_class(classID):
     """Functions for using student card scanning To attend class"""
     while True:
         cardID = input()
-        tsutc = datetime.utcnow() #UTC Timestamp
-        tsutx = calendar.timegm(tsutc.utctimetuple()) #covert UTC to UTX Timestamp
+        if cardID == "End":
+            terminate_program()
+        timestamp_utc = datetime.utcnow() #UTC Timestamp
+        timestamp_utx = calendar.timegm(timestamp_utc.utctimetuple()) #covert UTC to Unix Timestamp
         if cardID.isdigit() == True and len(cardID) == 10:
-            attend_api_post(classID, cardID, tsutx)
+            attend_api_post(classID, cardID, timestamp_utx)
         else:
             error()
 
 
-def attend_api_post(classID, cardID, timestamp):
+
+def attend_api_post(classID, cardID,timestamp):
     """Function to send student attendance API data to the web and receive data from the website"""
     param = {"ClassID": classID, "CardID": cardID, "Timestamp": timestamp}
     res = requests.post("https://api-t2o.ktnis.me", params=param)
@@ -60,6 +66,11 @@ def recive_paramita(res):
             winsound.Beep(2500, 350)
     else:
         error()
+
+
+def terminate_program():
+    """This function is used to stop the program"""
+    sys.exit()
 
 
 def error():
