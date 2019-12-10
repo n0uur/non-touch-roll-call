2c7b3ed8afa612f5464762b3fe1b7cdee792bbe2
hikariz01 <kittipak01@gmail.com>
2019-12-10 22:28:30 +0700

    ดึงข้อมูลจาก api

--- roll-call-web/src/components/Class/view/index.vue
+++ roll-call-web/src/components/Class/view/index.vue
@@ -8,7 +8,7 @@
               <div class="row">
                 <div class="col-md-12">
                   <div class="overview-wrap mb-2">
-                    <h2 class="title-1">จัดการห้องเรียน {{ classroom.id }}</h2>
+                    <h2 class="title-1">จัดการห้องเรียน {{ classID }}</h2>
                   </div>
                 </div>
               </div>
@@ -39,24 +39,30 @@
                   </div>
                 </div>
                 <transition-group name="slide-fade" tag="div" class="row">
-                  <div class="col-md-4 col-lg-3" v-for="i in studentList" :key="i">
+                  <div class="col-md-4 col-lg-3" v-for="std in studentList" :key="new Date(std.Attend_Time).getTime()">
                     <div class="card">
                       <div class="card-header">
                         <i class="fa fa-user"></i>
-                        <strong class="card-title pl-2">{{ i }}</strong>
+                        <strong class="card-title pl-2">{{ std.STD_ID }}</strong>
                       </div>
                       <div class="card-body">
                         <div class="mx-auto d-block">
                           <img
                             class="rounded-circle mx-auto d-block"
-                            src="https://media1.tenor.com/images/b9c59363e7dd35cb7c19d8736b102d99/tenor.gif?itemid=13730968"
-                            alt="Card image cap"
+                            style="height: 20vh; width: 20vh; object-fit: cover"
+                            :src="std.STD_IMG_URL"
+                            draggable="false"
+                            :alt="std.STD_Name + '\'s image'"
                           />
-                          <h4 class="text-sm-center mt-2 mb-1">พี่โต{{i}}</h4>
+                          <h4 class="text-sm-center mt-2 mb-1">{{ std.STD_Name + " " + std.STD_Lastname }}</h4>
                           <div class="location text-sm-left">
-                            <i class="fa fa-clock"></i> เวลาเข้าเรียน : -
+                            <i class="fa fa-clock"></i> เวลาเข้าเรียน : {{ getDisplayDate(std.Attend_Time) }}
                             <br />
                             <i class="fa fa-clock"></i> เวลาออก : -
+                            <br />
+                            <i class="far fa-check-square"></i> สถานะ :
+                            <button v-if="std.Attend_Status == 1" class="btn btn-sm btn-success">ปกติ</button>
+                            <button v-else class="btn btn-sm btn-danger">สาย</button>
                           </div>
                         </div>
                       </div>
@@ -81,26 +87,94 @@
 
 <script>
 import io from 'socket.io-client'
+import axios from 'axios'
 
 export default {
   data() {
     return {
       show: true,
-      classroom: {
-        id: this.$route.params.classid
-      },
+      classID: this.$route.params.classid,
+      classData: [],
       studentRaw: [],
+      studentInfo: [],
       classSocket: io("192.168.1.41:3010")
     };
   },
   computed: {
       studentList () {
-          return this.studentRaw.reverse()
+        // temp_list = []
+        // this.studentRaw.forEach(data => {
+        //   axios({
+        //     method: "GET",
+        //     url: "http://192.168.1.41:3000/std/getid/" + data.STD_ID,
+        //     data: []
+        //   })
+        //   .then((res) => {
+        //     data.push(res)
+        //     console.log(data)
+        //   })
+        //   .catch((err) => {
+        //     console.log(err)
+        //   });
+        // });
+        return this.studentRaw.reverse()
       }
   },
+  methods: {
+    updateStdData () {
+      let temp_list = []
+      axios({
+        method: "GET",
+        url: "http://192.168.1.41:3000/class/getstd/" + this.classID,
+        data: []
+      })
+      .then((res) => {
+        res.data.forEach(e => {
+          axios({
+            method: "GET",
+            url: "http://192.168.1.41:3000/std/getid/" + e.STD_ID,
+            data: []
+          })
+          .then((res) => {
+            temp_list.push(Object.assign({}, e, res.data))
+            // console.log(Object.assign({}, e, res.data))
+          })
+          .catch((err) => {
+            console.log(err)
+          });
+        })
+        this.studentRaw = temp_list
+        
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+    },
+    getDisplayDate(time) {
+      let dateTime = new Date(time)
+      return dateTime.getHours() + ":" + dateTime.getMinutes() + ":" + dateTime.getSeconds()
+    },
+    // getStudentInfo (stdID) {
+    //   axios({
+    //     method: "GET",
+    //     url: "http://192.168.1.41:3000/std/getid/" + stdID,
+    //     data: []
+    //   })
+    //   .then((res) => {
+    //     studentInfo.push(res)
+    //   })
+    //   .catch((err) => {
+    //     console.log(err)
+    //   });
+    // }
+  },
   mounted() {
-    this.socket.on('updateStd', (data) => {
-        this.studentRaw = [...this.studentRaw, data];
+    this.updateStdData()
+
+    this.classSocket.on('updateStd', (data) => {
+        if (data.classID == this.classID) { // not good but work <- must find the better way
+          this.updateStdData()
+        }
     });
   }
 };
