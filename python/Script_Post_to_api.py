83820ec03a6c564165523b38c0077061de2725d4
hikariz01 <kittipak01@gmail.com>
2019-12-11 02:31:13 +0700

    more beautiful code

--- python/Script_Post_to_api.py
+++ python/Script_Post_to_api.py
@@ -6,10 +6,9 @@ import sys
 import json
 from datetime import datetime
 
-def start():
+
+def start(classID, num_std_in_class):
     """Function to start the system by scanning to attend class"""
-    classID = input("Please fill in the classname:")
-    num_std_in_class = int(input("Please enter the number of students:"))
     if classID == "stdreg":
         stdreg()
     else:
@@ -20,11 +19,11 @@ def stdreg():
     """This function is used for registration. By student ID and student ID"""
     while True:
         cardID = input("Please tap the card:")
-        if cardID == "End":
+        if cardID.lower() == "end":
             terminate_program()
         studentID = input("Please enter the student id:")
-        if cardID.isdigit() == True and len(cardID) == 10 \
-            and studentID.isdigit() == True and len(studentID) == 8:
+        if cardID.isdigit() and len(cardID) == 10 \
+                and studentID.isdigit() and len(studentID) == 8:
             stdreg_api_post(cardID, studentID)
         else:
             error()
@@ -35,21 +34,23 @@ def attend_class(classID, num_std_in_class):
     num_std_attend = 0
     while True:
         cardID = input("Please tap the card:")
-        if cardID == "End":
-            percentage = percentage_attending_class(num_std_in_class, num_std_attend)
-            print("The number of students attending the class is %.2f percent of the total students."%percentage)
+        if cardID.lower() == "end":
+            percentage = percentage_attending_class(
+                num_std_in_class, num_std_attend)
+            print(
+                "The number of students attending the class is %.2f percent of the total students." % percentage)
             terminate_program()
         num_std_attend += 1
-        timestamp_utc = datetime.utcnow() #UTC Timestamp
-        timestamp_utx = calendar.timegm(timestamp_utc.utctimetuple()) #covert UTC to Unix Timestamp
-        if cardID.isdigit() == True and len(cardID) == 10:
+        timestamp_utc = datetime.utcnow()  # UTC Timestamp
+        # covert UTC to Unix Timestamp
+        timestamp_utx = calendar.timegm(timestamp_utc.utctimetuple())
+        if cardID.isdigit() and len(cardID) == 10:
             attend_api_post(classID, cardID, timestamp_utx)
         else:
             error()
 
 
-
-def attend_api_post(classID, cardID,timestamp):
+def attend_api_post(classID, cardID, timestamp):
     """Function to send student attendance API data to the web and receive data from the website"""
     param = {"ClassID": classID, "CardID": cardID, "Timestamp": timestamp}
     res = requests.post("https://api-t2o.ktnis.me", params=param)
@@ -57,13 +58,14 @@ def attend_api_post(classID, cardID,timestamp):
 
 
 def stdreg_api_post(cardID, studentID):
-    """This function is used to send the registration API data and receive data from the website."""
+    """This function is use for send the registration API data and receive data from the website."""
     param = {"CardID": cardID, "StudentID": studentID}
     res = requests.post("https://api-t2o.ktnis.me", params=param)
     recive_paramita(res)
 
 
 def recive_paramita(res):
+    """Function for status notifications"""
     if json.loads(res.text).get('status', 404) == 200:
         now = datetime.now()
         print("Success", now.strftime("%Y-%m-%d %H:%M:%S"))
@@ -77,7 +79,6 @@ def percentage_attending_class(num_std_in_class, num_std_attend):
     """The function calculates the student's attendance  as a percentage"""
     percentage = (num_std_attend/num_std_in_class)*100
     return percentage
-    
 
 
 def terminate_program():
@@ -90,4 +91,6 @@ def error():
     print("Error")
     winsound.Beep(2500, 750)
 
-start()
+
+start(input("Please fill in the classname:"), int(
+    input("Please enter the number of students:")))
