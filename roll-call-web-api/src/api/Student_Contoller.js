053c72a714a27637d0f771e2ae16110ed16b69f5
Krittanut Siripornnoppakhun <50010805+n0uur@users.noreply.github.com>
2019-12-12 22:28:02 +0700

    add register api function

--- roll-call-web-api/src/api/Student_Contoller.js
+++ roll-call-web-api/src/api/Student_Contoller.js
@@ -36,21 +36,53 @@ StudentController.get('/getid/:stdid', (req, res) => {
     }
 })
 
-StudentController.get('/register', (req, res) => {
-    try {
-        const { cardid } = req.params
+// StudentController.get('/register', (req, res) => {
+//     try {
+//         const { cardid } = req.params
 
-        conn.query("SELECT * from student_data WHERE STD_CardID = ?", [cardid], (error, results, fields) => {
-            if (error) {
-                console.log(error)
+//         conn.query("SELECT * from student_data WHERE STD_CardID = ?", [cardid], (error, results, fields) => {
+//             if (error) {
+//                 console.log(error)
+//             }
+//             res.header("Content-Type", 'application/json')
+//             res.status(200).send(JSON.stringify(results[0], null, 2))
+//         })
+//     }
+//     catch (e) {
+//         console.log(e)
+//     }
+// })
+
+StudentController.post('/register', (req, res) => {
+    const { cardid, studentid } = req.body
+
+    // console.log(req.body)
+
+    conn.query("SELECT * from student_data WHERE STD_ID = ?", [studentid], (error, results, fields) => {
+        if (error) {
+            console.log(error)
+            res.status(500).send()
+        }
+        else {
+            // console.log(results.length)
+            if (results.length) {
+                conn.query("UPDATE student_data set STD_CardID = ? WHERE STD_ID = ?", [cardid, studentid], (error, results, fields) => {
+                    if (error) {
+                        console.log(error)
+                        res.status(500).send()
+                    }
+                    else {
+                        res.header("Content-Type", 'application/json')
+                        res.status(200).send({status: 200, message: 'register success'})
+                    }
+                })
             }
-            res.header("Content-Type", 'application/json')
-            res.status(200).send(JSON.stringify(results[0], null, 2))
-        })
-    }
-    catch (e) {
-        console.log(e)
-    }
+            else {
+                res.header("Content-Type", 'application/json')
+                setTimeout((function() { res.status(200).send({status: 404, message: 'student id not found'}) }), 500);
+            }
+        }
+    })
 })
 
 StudentController.get('/getall', (req, res) => {
