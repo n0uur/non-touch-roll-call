cab2453597f6ac3d7e43457e0ed1306d7eae947b
n0uur <50010805+n0uur@users.noreply.github.com>
2019-12-12 18:34:44 +0700

    fix responsive

--- roll-call-web/src/components/Class/view/all.vue
+++ roll-call-web/src/components/Class/view/all.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="container bg-light mt-3">
             <div class="row mt-3 mb-3">
-                <div class="col">
+                <div class="col mt-2">
                     <h3><i class="fas fa-users"></i> หน้ารวมห้องเรียน <div class="text-right"><button class="btn btn-success" @click="$router.push({ name: 'Create_1'})"><i class="fas fa-plus"></i> สร้างห้องเรียน</button></div></h3>
                 </div>
             </div>
